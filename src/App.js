70849e24c350bddc93260cbf483534b68bf5d130
geumyoung <1gyn093@gmail.com>
2023-10-29 00:51:44 +0900

    컴포넌트 호출

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import Counter from './components/Counter';
+import Header from './components/Header'
+import Auth from './components/Auth'
+
 
 function App() {
-  return <Counter />;
+  return (
+    <>
+      <Header />
+      <Auth />
+      <Counter />
+    </>
+  )
+
 }
 
 export default App;
